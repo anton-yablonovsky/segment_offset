fc7bc28439c8251e1a4bc54e4dcdcfedb49fd95e
Anton Yablonovsky <anton.yablonovsky@gmail.com>
2023-12-01 09:17:01 +0200

    Refactored, new conditions to segment_direction are added

--- classes/segment.py
+++ classes/segment.py
@@ -7,17 +7,48 @@ class Segment:
         self.end_point = end_point
 
     def segment_direction(self) -> int:
-        if self.start_point.y < self.end_point.y:
-            return 1  # AB: A(0,0)-B(2,2). Direction is from down to up.
-        elif self.start_point.y > self.end_point.y:
-            return -1  # AB: A(2,2)-B(0,0). Direction is from up to down.
-        else:
-            return 0  # AB: A(0,0)-B(2,0). Line is parallel Ox.
+        if (
+            self.start_point.x < self.end_point.x
+            and self.start_point.y < self.end_point.y
+        ):
+            return -1
+
+        elif (
+            self.start_point.x > self.end_point.x
+            and self.start_point.y > self.end_point.y
+        ):
+            return 1
+
+        elif (
+            self.start_point.x < self.end_point.x
+            and self.start_point.y > self.end_point.y
+        ):
+            return -1
+
+        elif (
+            self.start_point.x > self.end_point.x
+            and self.start_point.y < self.end_point.y
+        ):
+            return 1
+
+        elif (
+            self.start_point.x == self.end_point.x
+            and self.start_point.y < self.end_point.y
+        ):
+            return 1
+
+        elif (
+            self.start_point.x == self.end_point.x
+            and self.start_point.y > self.end_point.y
+        ):
+            return -1
 
     def get_slope(self) -> float:
+
         if self.start_point.x == self.end_point.x:
             # If the line is vertical, set slope to None (undefined)
             return None
+
         else:
             return (self.end_point.y - self.start_point.y) / (
                 self.end_point.x - self.start_point.x
