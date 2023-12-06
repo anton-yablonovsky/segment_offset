0e6483b5fe0265aeec421947ade71a0e302291c4
Anton Yablonovsky <anton.yablonovsky@gmail.com>
2023-12-06 05:59:55 +0200

    Methods for add, div and mul are added

--- classes/point.py
+++ classes/point.py
@@ -8,3 +8,19 @@ class Point:
 
     def convert(self) -> Tuple:
         return self.x, self.y
+    
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+    
+    def __truediv__(self, scalar):
+        try:
+            x = self.x / scalar
+            y = self.y / scalar
+        except:
+            return False
+
+        return Point(x, y)
+    
+    def __mul__(self, scalar):
+        return Point(self.x * scalar, self.y * scalar)
+
